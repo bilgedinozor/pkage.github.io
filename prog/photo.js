cf1406f807a46e43c1a0494d101ddb65bc0001bc
Ali Can KISMET <alicankismet@hotmail.com>
2024-02-21 02:12:06 +0300

    add coach

--- prog/photo.js
+++ prog/photo.js
@@ -3,23 +3,30 @@ class PhotoProgram extends Program {
         let title = (argument === null) ? 'Photo' : `Photo (${argument})`
 
         let winfo = {
-            name: 'Photo',
+            name: 'Realy ?',
             title,
             icon: 'img/taskbar/Paint.png',
             resizable: false,
             margin: false,
             app: true
         }
-
-        // eventually replace with fs driver...
-        let doc = ''
-        if (argument == 'coach.png') {
-            doc = Coach
-            winfo.width = 634
-            winfo.height = 483
-        }
-
         let body = `
+        <div class="menu-bar__container">
+                <div class="menu-bar__menu">
+                    <div class="menu-bar__handle"></div>
+                    <span class="menu-bar__item">
+                        File
+                    </span>
+                    <span class="menu-bar__item">
+                        Edit
+                    </span>
+                    <span class="menu-bar__item">
+                        Search
+                    </span>
+                    <span class="menu-bar__item">
+                        Help
+                    </span>
+                </div>
         <div
         class="window"      
         data-wm_order="0"
